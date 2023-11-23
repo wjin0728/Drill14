528dc423df86e67b019503621185c9775352a774
woojin <wjin0728@naver.com>
2023-11-23 13:01:02 +0900

    drill14 제출 (2022180031 이우진)

---
+++ play_mode.py
@@ -0,0 +1,63 @@
+import random
+import json
+import os
+
+from pico2d import *
+import game_framework
+import game_world
+
+import server
+from boy import Boy
+from background import FixedBackground as Background
+from  ball import Ball
+
+# fill here
+
+
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        else:
+            server.boy.handle_event(event)
+
+
+
+def init():
+    server.background = Background()
+    game_world.add_object(server.background, 0)
+    server.boy = Boy(400, 300)
+    game_world.add_object(server.boy, 1)
+    game_world.add_collision_pair('boy:ball', server.boy, None)
+    balls = [Ball() for _ in range(1000)]
+    game_world.add_objects(balls, 1)
+    for ball in balls:
+        game_world.add_collision_pair('boy:ball', None, ball)
+
+def finish():
+    game_world.clear()
+    pass
+
+
+def update():
+    game_world.update()
+    game_world.handle_collisions()
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+def pause():
+    pass
+
+def resume():
+    pass
+
+
+
